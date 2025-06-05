a4e81371dd8d4581729aaf4fd9ce5a0705fac592
deep-amol2209 <anmol222006@gmail.com>
2025-06-05 21:40:44 +0530

    update config

--- backend/config/index.js
+++ backend/config/index.js
@@ -9,6 +9,15 @@ module.exports = {
     clientSecret: process.env.ZOOM_CLIENT_SECRET,
     accountId: process.env.ZOOM_ACCOUNT_ID
     },
+    email: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
+        },
+        from: process.env.EMAIL_FROM,
+      },
     jwt:{
         accessSecret:process.env.JWT_ACCESS_SECRET || "random-secret",
         accessExpirationMinutes:process.env.JWT_ACCESS_EXPIRATION_MINUTES || 30,

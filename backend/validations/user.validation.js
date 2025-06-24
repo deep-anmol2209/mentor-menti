00d74426538784f727dc897b6927a76fe86901f6
deep-amol2209 <anmol222006@gmail.com>
2025-06-24 22:11:47 +0530

    resolve nested feilds in edit profile

--- backend/validations/user.validation.js
+++ backend/validations/user.validation.js
@@ -6,11 +6,12 @@ const updateUserProfileValidation = Joi.object({
     title: Joi.string().optional(),
     bio: Joi.string().optional(),
     social: Joi.object({
-        linkedin: Joi.string().optional(),
-        github: Joi.string().optional(),
-        twitter: Joi.string().optional(),
-        facebook: Joi.string().optional(),
-        instagram: Joi.string().optional(),
+       
+        linkedin:Joi.string().allow('', null).optional(),
+        github:Joi.string().allow('', null).optional(),
+        twitter: Joi.string().allow('', null).optional(),
+        facebook:Joi.string().allow('', null).optional(),
+        instagram: Joi.string().allow('', null).optional(),
     }).optional(),
     college: Joi.string().optional(),
 });
